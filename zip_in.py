ff292f403659ac23c9d6bbfd9100edee04b396e5
sysnote8 <sysnoteser@gmail.com>
2022-04-11 15:25:45 +0900

    edit zip_in.py

--- zip_in.py
+++ zip_in.py
@@ -1,7 +1,6 @@
 import shutil
 
 #デタパの階層にZIPする。
-shutil.make_archive('resources','zip',root_dir='C:\\Users\\kirig\\Desktop\\MC\\saves\\メンドクサ\\datapacks\\NNSB_main_system\\resources')
-
-#zipフォルダを移動する。
-shutil.move('C:\\Users\\kirig\\Desktop\\MC\\saves\\メンドクサ\\datapacks\\NNSB_main_system\\resources.zip','C:\\Users\\kirig\\Desktop\\MC\\saves\\メンドクサ')
+shutil.make_archive("resources",format="zip",root_dir="../NNSB_main_system")
+#データパックの移動
+shutil.move("./resources.zip","../resources.zip")
