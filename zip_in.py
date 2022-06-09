e94f47e3a997c2af9de081c505f9fdb664e1bd98
sysnote8main <69232601+sysnote8main@users.noreply.github.com>
2022-06-09 21:44:55 +0900

    [release] test release
    This is release test.

--- zip_in.py
+++ zip_in.py
@@ -1,6 +1,6 @@
 import shutil
 
-#デタパの階層にZIPする。
+#データパックの階層にZIPする。
 shutil.make_archive("resources",format="zip",root_dir="../NNSB_main_system")
 #データパックの移動
 shutil.move("./resources.zip","../resources.zip")
